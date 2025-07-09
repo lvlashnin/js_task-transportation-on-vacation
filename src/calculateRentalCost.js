3c09102d8365ce7aea6de25b8986c902467e6759
lvlashnin <lvlashnin@gmail.com>
2025-07-09 11:31:36 +0300

    added discount logic

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,25 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let resultCost = 0;
+
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const UNDISCOUNTED_COST = days * 40;
+
+  if (days < 3) {
+    resultCost = UNDISCOUNTED_COST;
+  }
+
+  if (days >= 3 && days < LONG_TERM) {
+    resultCost = UNDISCOUNTED_COST - 20;
+  }
+
+  if (days >= LONG_TERM) {
+    resultCost = UNDISCOUNTED_COST - LONG_TERM_DISCOUNT;
+  }
+
+  return resultCost;
 }
 
 module.exports = calculateRentalCost;
