45f3b53793c0579036b594916d6b7d49da7bddd2
lvlashnin <lvlashnin@gmail.com>
2025-07-09 14:34:43 +0300

    fixed the issues according to the remarks of the mate

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,25 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let resultCost = 0;
-
   const LONG_TERM = 7;
+  const SHORT_TERM = 3;
   const LONG_TERM_DISCOUNT = 50;
-  const UNDISCOUNTED_COST = days * 40;
-
-  if (days < 3) {
-    resultCost = UNDISCOUNTED_COST;
-  }
-
-  if (days >= 3 && days < LONG_TERM) {
-    resultCost = UNDISCOUNTED_COST - 20;
-  }
+  const SHORT_TERM_DISCOUNT = 20;
+  const PRICE_A_DAY = 40;
+  const RESULT_COST = PRICE_A_DAY * days;
 
   if (days >= LONG_TERM) {
-    resultCost = UNDISCOUNTED_COST - LONG_TERM_DISCOUNT;
+    return RESULT_COST - LONG_TERM_DISCOUNT;
   }
 
-  return resultCost;
+  if (days >= SHORT_TERM) {
+    return RESULT_COST - SHORT_TERM_DISCOUNT;
+  }
+
+  return RESULT_COST;
 }
 
 module.exports = calculateRentalCost;
